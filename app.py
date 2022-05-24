1bc94637f640f174cff68c2e1d790bd1fb6bd5d2
SSOhana <v0uu0a@gmail.com>
2022-05-24 12:37:23 +0900

    add

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from fbprophet import Prophet
+
+def main() :
+    pass
+
+if __name__ == 'main' :
+    main()
+
+
+## 파이썬 가상 환경을 확인한다!
+## 이 앱은 구글 코랩에서 만든 아보카도 가격 예측 환경과
+## 동일하게 하기 위해서
+## 파이썬 가상환경을 streamlit3.7로 바꿔준다.
+## => vs code 맨 아래쪽에 있는 base 를 streamlit3.7 로 변경po
