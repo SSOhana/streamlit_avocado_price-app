2b133bc77ea1c8d72ff190bdec4acdc66874ff1e
SSOhana <v0uu0a@gmail.com>
2022-05-24 18:01:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ if __name__ == 'main' :
 ## 이 앱은 구글 코랩에서 만든 아보카도 가격 예측 환경과
 ## 동일하게 하기 위해서
 ## 파이썬 가상환경을 streamlit3.7로 바꿔준다.
-## => vs code 맨 아래쪽에 있는 base 를 streamlit3.7 로 변경po
+## => vs code 맨 아래쪽에 있는 base 를 streamlit3.7 로 변경
